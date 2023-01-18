0393cf5470526835a87b776f8fd9c4b104b103b0
fajarabdillahfn <fajar.abdillahfn@gmail.com>
2023-01-18 21:36:36 +0700

    fix: json display name for model

--- internal/model/movie.go
+++ internal/model/movie.go
@@ -6,12 +6,12 @@ import (
 )
 
 type Movie struct {
-	ID          uint   `gorm:"primaryKey"`
-	Title       string `gorm:"not null"`
-	Description string
-	Rating      float32
-	Image       string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	ID          uint           `gorm:"primaryKey" json:"id"`
+	Title       string         `gorm:"not null" json:"title"`
+	Description string         `json:"description"`
+	Rating      float32        `json:"rating"`
+	Image       string         `json:"image"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
 	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
 }
